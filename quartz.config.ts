0ce051eeb8b3fe5b30c6a16c9c4f45e48c7c9b8d
Nihit Nirmal <nihit@mac.lan>
2026-01-22 22:21:17 +0800

    Update custom domain to nihitnirmal.com

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "nihit.me",
+    baseUrl: "nihitnirmal.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
